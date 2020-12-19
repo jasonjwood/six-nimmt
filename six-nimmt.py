c4b34b88491394c7b62b2ff424991efa5b603901
Jason Wood <jason.j.wood@gmail.com>
2020-12-18 23:27:55 -0500

    Reading state

--- six-nimmt.py
+++ six-nimmt.py
@@ -1,7 +1,26 @@
 
+hand = []
+table = []
 
 
+def read_state():
+    global hand
+    global table
 
+    with open('state.txt', 'r') as state_file:
+        lines = state_file.readlines()
+
+    # Parse hand
+    hand = lines[1].replace('\n', '').split('\t')
+
+    # Parse table rows
+    table = []
+    for i in range(4, 8):
+        table.append(lines[i].replace('\n', '').split('\t'))
+
+    assert (len(table) == 4)
+
+    print(table)
 
 
 def main():
@@ -11,9 +30,12 @@ def main():
         input('Press enter when state.txt is populated...')
 
         # Read in state
+        read_state()
 
         # Decide which card to play
 
+
+        # NOTE: Start with no memory of played cards
 
 
 main()
