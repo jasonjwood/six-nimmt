c5b9edd0fc6e3f63e471ed1532e5afab28e31ab2
Jason Wood <jason.j.wood@gmail.com>
2020-12-18 22:21:23 -0500

    First

---
+++ six-nimmt.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+def main():
+    # Loop for duration of one deal of the deck
+    while True:
+        # Wait for key to indicate input is ready
+        input('Press enter when state.txt is populated...')
+
+        # Read in state
+
+        # Decide which card to play
+
+
+
+main()
